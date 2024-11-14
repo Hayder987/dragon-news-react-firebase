642135a4a97fed0addb1ecfa54ae56559e5a73bc
hayder <hayderbd4290@gmail.com>
2024-11-15 01:29:22 +0600

    marquee news Update

--- src/components/HomeComponents/header/HeaderMarquee.jsx
+++ src/components/HomeComponents/header/HeaderMarquee.jsx
@@ -1,17 +1,27 @@
+import { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 
 const HeaderMarquee = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+     fetch("https://openapi.programming-hero.com/api/news/category/01")
+     .then(res=> res.json())
+     .then(data=> setData(data.data))
+  },[])
+
+  
+
     return (
         <div className="flex items-center gap-4 bg-gray-200 p-3">
             <button className="bg-[#D72050] text-white text-xl font-medium px-4 py-2">Latest</button>
             <Marquee pauseOnHover={true} speed={100}>
               <div className="flex gap-10">
-                <p className="font-semibold">Match Highlights: Germany vs Spain — as it
-                   happened   ! 
-                    Match Highlights: Germany vs Spain as...</p>
-                    <p className="font-semibold"> it
-                   happened   ! 
-                    Match Highlights: Germany vs Spain as...</p>
+                {
+                  data.map(item=>(
+                    <p key={item._id} className="">{item.title}</p>
+                  ))
+                }
               </div>
             </Marquee>
             
